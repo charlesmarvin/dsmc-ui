72d088d11ca40bc58d7228a0d64da2365278b7e4
charlesmarvin <marvin.charles@gmail.com>
2016-11-26 16:58:55 -0500

    Student list style cleanup

--- src/components/students/StudentList.js
+++ src/components/students/StudentList.js
@@ -7,42 +7,49 @@ class StudentList extends Component {
       this.props.dispatch(load());
 	}
 
-  getPreferredContactNumber(contactNumbers) {
-    if (!contactNumbers) {
+  renderPhoneContactList(contactNumbers) {
+    if(!contactNumbers || contactNumbers.length === 0) {
       return '';
     }
-    let mobile = contactNumbers.find((n) => n.type === 'Mobile' );
-    if (mobile) {
-      return mobile.number;
-    }
-    let home = contactNumbers.find((n) => n.type === 'Home' );
-    if (home) {
-      return home.number;
-    }
-    let work = contactNumbers.find((n) => n.type === 'Work' );
-    if (work) {
-      return work.number;
-    }
+    return (
+      <select> 
+      {contactNumbers.map((number, idx) => {
+          return (<option value={number.number} key={idx}>
+            {number.number} - {number.type}
+          </option>);
+        })}
+      </select>
+    );
   }
   render() {
       return (
         <div className="row">
           <div className="col-xs">
             <div className="content">
-              <h3>Students</h3>            
-              <section className="">
+              <h3>Students</h3> 
+              <div className="student-list">
                 {this.props.students.map((student) => {
                   return (
-                    <section className="student" key={student.id}>
-                      <div className="row">
-                        <div className="col-xs-12">
-                          {student.firstName} - {this.getPreferredContactNumber(student.contactNumbers)}
+                    <div className="row student-list-item" key={student.id}>
+                      <div className="col-xs-12 col-sm-4">
+                        <div className="box">
+                          {student.firstName} {student.lastName}
                         </div>
                       </div>
-                    </section>
+                      <div className="col-xs-12 col-sm-4">
+                        <div className="box">
+                          {student.email}
+                        </div>
+                      </div>
+                      <div className="col-xs-12 col-sm-4">
+                        <div className="box">
+                          {this.renderPhoneContactList(student.contactNumbers)}
+                        </div>
+                      </div>
+                    </div>
                   );
                 })}
-              </section>		
+              </div>
             </div>
           </div>
         </div>
